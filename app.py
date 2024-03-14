8882a0d701319179995129ea4545790001d4a872
Karthik Soman <karthi.soman@gmail.com>
2024-03-14 13:20:47 -0700

    updated app to include only UCSF

--- app.py
+++ app.py
@@ -7,15 +7,26 @@ import streamlit as st
 patient_profile = pd.read_csv('data/patient_profile_site_included.csv')
 patient_feature_array = patient_profile.drop(['CLIENT_SAMPLE_ID', 'gARMSS', 'armms_category'], axis=1).to_numpy()
 
+xmin = patient_profile.tsne1.min()-1
+xmax = patient_profile.tsne1.max()+1
+ymin = patient_profile.tsne2.min()-1
+ymax = patient_profile.tsne2.max()+1
 
 
 # Define colors for armms_category
 color_map = {'Lower ARMSS': 'green', 'Higher ARMSS': 'red'}
 
-# Plot
-fig = px.scatter(patient_profile, x='tsne1', y='tsne2', color='armms_category', color_discrete_map=color_map,
-                 hover_data={'tsne1': False, 'tsne2': False, 'CLIENT_SAMPLE_ID': True, 'gARMSS': True}
-                )
+ucsf_check = st.checkbox('UCSF only')
+if not ucsf_check:
+    # Plot
+    fig = px.scatter(patient_profile, x='tsne1', y='tsne2', color='armms_category', color_discrete_map=color_map,
+                     hover_data={'tsne1': False, 'tsne2': False, 'CLIENT_SAMPLE_ID': True, 'gARMSS': True}
+                    )
+else:
+    patient_profile_ucsf_only = patient_profile[patient_profile.CLIENT_SAMPLE_ID.str.startswith('714')]
+    fig = px.scatter(patient_profile_ucsf_only, x='tsne1', y='tsne2', color='armms_category', color_discrete_map=color_map,
+                     hover_data={'tsne1': False, 'tsne2': False, 'CLIENT_SAMPLE_ID': True, 'gARMSS': True}
+                    )
 
 # Update legend names
 fig.for_each_trace(lambda t: t.update(name=color_map[t.name]))
@@ -37,57 +48,17 @@ fig.update_layout(
     xaxis=dict(
         title=dict(
             font=dict(size=20)  # Adjust the font size of the x axis label here
-        )
+        ),
+        range=[xmin, xmax]
     ),
     yaxis=dict(
         title=dict(
             font=dict(size=20)  # Adjust the font size of the y axis label here
-        )
+        ),
+        range=[ymin, ymax]
     )
 )
 # Display the plot
 st.plotly_chart(fig)
 
 
-# def plot_scatter(df):
-#     fig = px.scatter(df, x='tsne1', y='tsne2', color='armms_category',
-#                      color_discrete_map={1: 'red', 0: 'green'},
-#                      hover_data={'tsne1': False, 'tsne2': False,
-#                                  'CLIENT_SAMPLE_ID': True, 'gARMSS': True},
-#                      labels={'CLIENT_SAMPLE_ID': 'Client Sample ID', 'gARMSS': 'gARMSS'})
-#     fig.update_traces(marker=dict(size=10))
-#     fig.update_layout(title='tSNE Plot',
-#                       xaxis_title='tSNE 1',
-#                       yaxis_title='tSNE 2',
-#                       legend_title='ARMSS Category',
-#                       )
-#     return fig
-
-
-# def plot_scatter(df):
-#     fig = px.scatter(df, x='tsne1', y='tsne2', color='armms_category',
-#                      color_discrete_map={1: 'red', 0: 'green'},
-#                      hover_data={'tsne1': False, 'tsne2': False,
-#                                  'CLIENT_SAMPLE_ID': True, 'gARMSS': True},
-#                      labels={'CLIENT_SAMPLE_ID': 'Client Sample ID', 'gARMSS': 'gARMSS'})
-#     fig.update_traces(marker=dict(size=10))
-#     fig.update_layout(title='tSNE Plot',
-#                       xaxis_title='tSNE 1',
-#                       yaxis_title='tSNE 2')
-#     # Add legend
-#     fig.update_layout(legend=dict(
-#         title="ARMSS Category",
-#         items=[
-#             dict(label="Higher gARMSS", marker=dict(color='red', size=10)),
-#             dict(label="Lower gARMSS", marker=dict(color='green', size=10)),
-#         ]
-#     ))
-#     return fig
-
-
-# def main():
-#     st.title("tSNE Plot of patient profile")
-#     st.plotly_chart(plot_scatter(patient_profile))
-
-# if __name__ == "__main__":
-#     main()
